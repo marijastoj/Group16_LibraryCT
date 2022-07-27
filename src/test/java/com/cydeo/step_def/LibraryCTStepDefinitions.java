a56033e591c57f83bca3c24621b5ff0113f43196
KennethB824 <kenneth.bermudez001@mymdc.net>
2022-07-27 16:32:12 -0400

    added move to element to BrowserUtils

---
+++ src/test/java/com/cydeo/step_def/LibraryCTStepDefinitions.java
@@ -0,0 +1,59 @@
+package com.cydeo.step_def;
+
+import com.cydeo.pages.LibraryCTBookPage;
+import com.cydeo.pages.LibraryCTLoginPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class LibraryCTStepDefinitions {
+
+    LibraryCTLoginPage libraryCTLoginPage = new LibraryCTLoginPage();
+    LibraryCTBookPage libraryCTBookPage = new LibraryCTBookPage();
+
+    @Given("librarian is on the loginPage")
+    public void librarian_is_on_the_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+    }
+    @When("librarian enters valid credentials and clicks sign in button")
+    public void librarian_enters_valid_credentials_and_clicks_sign_in_button() {
+        libraryCTLoginPage.login(ConfigurationReader.getProperty("librarianUsername"), ConfigurationReader.getProperty("librarianPassword"));
+    }
+    @Given("librarian is on homePage")
+    public void librarian_is_on_home_page() {
+        BrowserUtils.waitForTitle("Library");
+        BrowserUtils.verifyTitle("Library");
+    }
+    @When("librarian clicks Books module")
+    public void librarian_clicks_books_module() {
+        libraryCTBookPage.booksModuleBtn.click();
+    }
+    @When("librarian clicks +Add Book button")
+    public void librarian_clicks_button() {
+        libraryCTBookPage.addBookBtn.click();
+    }
+    @When("librarian enters BookName, ISBN, Year,Author and Description")
+    public void librarian_enters_book_name_isbn_year_author_and_description() {
+        BrowserUtils.waitForClickable(libraryCTBookPage.bookNameBox);
+        libraryCTBookPage.bookNameBox.sendKeys("Eating Cucumbers 2: More Cucumbers");
+        libraryCTBookPage.isbnBox.sendKeys("98777");
+        libraryCTBookPage.yearBox.sendKeys("2021");
+        libraryCTBookPage.authorBox.sendKeys("Aysun");
+        libraryCTBookPage.descriptionBox.sendKeys("Cucumber is really fun!");
+    }
+    @When("librarian clicks save changes")
+    public void librarian_clicks_save_changes() {
+        libraryCTBookPage.saveChangesBtn.click();
+    }
+    @Then("verify a new book is added")
+    public void verify_a_new_book_is_added() {
+        Assert.assertTrue("Book created confirmation message has not appeared", libraryCTBookPage.bookCreatedMessage.isDisplayed());
+
+        System.out.println(libraryCTBookPage.bookCreatedMessage.getText());
+    }
+
+}
