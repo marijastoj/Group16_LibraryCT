5f55ecc83840c3ed05550c94cc8f2c4d2f226db3
marij <marijastoj@gamil.com>
2022-08-01 18:55:34 -0400

    Marija's tc

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_def",
         dryRun = false,
-        tags = "@B26G16-71"
+        tags = "@B26G16-66"
 
 )
 
